96f543eb3371f92cd39dba5f6455c641d8635741
rajabinekoo <ali.rajabinekoo@gmail.com>
2025-05-14 16:08:29 +0330

    Active liquidity logic refactored by multicall operation

--- scripts/active-liquidity.ts
+++ scripts/active-liquidity.ts
@@ -1,8 +1,16 @@
 import { writeFile } from "fs/promises";
-import { getTickInfo } from "./tick-info";
-import { TickInfo } from "../dtos/tick-info";
 import { SortedMap } from "../libs/sorted-map";
-import { int24Max, int24Min, tickSpacing } from "../libs/config";
+import { generateFetchingTickInfoCall } from "./tick-info";
+import { TickInfo, viemTickInfoType } from "../dtos/tick-info";
+import {
+  int24Max,
+  int24Min,
+  startBlock,
+  tickSpacing,
+  publicClient,
+} from "../libs/config";
+
+const multicallSize = 100 * tickSpacing;
 
 const getTicksRange = () => {
   const ranges: number[] = [];
@@ -12,19 +20,38 @@ const getTicksRange = () => {
 };
 
 export async function fetchAndSaveActiveLiquidities(filename: string) {
-  const initializedTicks = new SortedMap<number, TickInfo>((a, b) => a - b);
   const ticksRange = getTicksRange();
   const tickInfoList: Array<{ tick: number; info: TickInfo }> = [];
-  for (const t of ticksRange) {
-    console.log("Fetching ", t);
-    const info = await getTickInfo(t);
-    if (!info) {
-      console.log(`System failed to fetch tick info for ${t}`);
-      throw new Error("Something went wrong");
-    }
-    if (!info.initialized) continue;
-    tickInfoList.push({ tick: t, info });
+  const initializedTicks = new SortedMap<number, TickInfo>((a, b) => a - b);
+
+  // windows size = page = iteration
+  let iteration = 0;
+  const endIteration = Math.ceil(ticksRange.length / multicallSize);
+  console.log("Number of iterations:", endIteration);
+
+  while (iteration < endIteration) {
+    const startOfSlice = iteration * multicallSize;
+    const endOfSlice = Math.min(
+      startOfSlice + multicallSize,
+      ticksRange.length - 1
+    );
+    const rangeSlice = ticksRange.slice(startOfSlice, endOfSlice + 1);
+    const result = await publicClient.multicall({
+      blockNumber: startBlock,
+      contracts: rangeSlice.map((t) => generateFetchingTickInfoCall([t])),
+    });
+    if (!Array.isArray(result)) throw new Error("Something went wrong");
+    result.forEach((r, i) => {
+      if (r.status !== "success") throw new Error("Something went wrong");
+      const info = new TickInfo(<viemTickInfoType>r.result);
+      if (!info.initialized) return;
+      const t = ticksRange[i];
+      tickInfoList.push({ tick: t, info });
+    });
+    console.log(`Iteration ${iteration} done.`);
+    iteration += 1;
   }
+
   for (const { info, tick } of tickInfoList) initializedTicks.add(tick, info);
   await writeFile(filename, initializedTicks.toJSON(), "utf8");
 }
--- scripts/tick-info.ts
+++ scripts/tick-info.ts
@@ -1,3 +1,5 @@
+import { Address } from "viem";
+
 import { UniswapV3Pool } from "../abis/UniswapV3Pool";
 import { TickInfo, viemTickInfoType } from "../dtos/tick-info";
 import { poolAddress, publicClient, startBlock } from "../libs/config";
@@ -15,4 +17,18 @@ export async function getTickInfo(tick: number | bigint | string) {
   } catch (error) {
     console.error(error);
   }
+}
+
+export function generateFetchingTickInfoCall(args: Array<any>): {
+  abi: any;
+  args: Array<any>;
+  address: Address;
+  functionName: string;
+} {
+  return {
+    args,
+    address: poolAddress,
+    functionName: "ticks",
+    abi: UniswapV3Pool.abi,
+  };
 }
